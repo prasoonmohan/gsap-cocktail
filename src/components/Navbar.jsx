ea3dfc048baa2ed4eb73ba2d1c02c3b50776778e
Prasoon M <prasoon@aufaitux.com>
2025-06-25 21:06:09 +0530

    feat : Added Navbar background animation when we scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
+import { useGSAP } from '@gsap/react'
 import { navLinks } from '../../constants/index.js'
+import gsap from 'gsap'
+import { useRef } from 'react'
 
 const Navbar = () => {
+
+	const navRef = useRef(null)
+
+	useGSAP(() => {
+
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: navRef.current,
+				start: "bottom top",
+				scrub: true,
+				backdropFilter: "blur(0px)",
+			}
+		});
+
+		navTween.fromTo(navRef.current, { backgroundColor: "transparent", },
+			{
+				backgroundColor: '#00000050',
+				duration: 1,
+				backdropFilter: 'blur(10px)',
+				ease: "power1.inOut",
+			}
+		);
+
+	}, [])
+
 	return (
-		<nav>
+		<nav ref={navRef}>
 			<div>
 				<a href="#home" className="flex items-center gap-2">
 					<img src="/images/logo.png" alt="logo" />
