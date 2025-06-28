45ef98a99c91114ed5557aa402e8e09fbd4d48e5
Prasoon M <prasoon@aufaitux.com>
2025-06-28 12:48:26 +0530

    feat : Added animation in art section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,6 +1,38 @@
+import { useMediaQuery } from 'react-responsive'
 import { featureLists, goodLists } from '../../constants/index.js'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Art = () => {
+
+	const isMobile = useMediaQuery({ maxWidth: 756 });
+
+	useGSAP(() => {
+
+		const start = isMobile ? 'top 20%' : 'top top'
+		const maskedTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: '#art',
+				start,
+				end: 'bottom center',
+				scrub: 1.5,
+				pin: true
+			}
+		})
+
+		maskedTimeline
+			.to('.will-fade', {
+				opacity: 0, stagger: 0.2, ease: 'power1.inOut'
+			})
+			.to('.masked-img', {
+				scale: 1.3, maskPosition: 'center', maskSize: '400%', duration: 1, ease: 'power1.inOut'
+			})
+			.to('#masked-content', {
+				opacity: 1, ease: 'power1.inOut', duration: 1
+			})
+
+	}, [])
+
 	return (
 		<div id="art">
 			<div className="container mx-auto h-full pt-20">
