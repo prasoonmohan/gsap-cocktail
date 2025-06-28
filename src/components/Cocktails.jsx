0e5aa438343629bf1abdf41592ae038c741cb7d2
Prasoon M <prasoon@aufaitux.com>
2025-06-28 11:34:21 +0530

    feat : Added animation in cocktails section

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -1,11 +1,35 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import { cocktailLists, mockTailLists } from '../../constants/index.js'
+import { ScrollTrigger } from 'gsap/all';
+import { useRef } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Cocktails = () => {
+	const sectionRef = useRef(null);
+	const leftLeafRef = useRef(null);
+	const rightLeafRef = useRef(null);
+
+	useGSAP(() => {
+		const parallaxTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: sectionRef.current,
+				start: 'top 30%',
+				end: 'bottom 80%',
+				scrub: true
+			}
+		});
+
+		parallaxTimeline
+			.from(leftLeafRef.current, { x: -100, y: 100, ease: 'none' })
+			.from(rightLeafRef.current, { x: 100, y: 100, ease: 'none' });
+	}, []);
 
 	return (
-		<section id="cocktails" className="noisy">
-			<img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
-			<img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+		<section id="cocktails" className="noisy" ref={sectionRef}>
+			<img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" ref={leftLeafRef} />
+			<img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" ref={rightLeafRef} />
 
 			<div className="list">
 				<div className="popular">
