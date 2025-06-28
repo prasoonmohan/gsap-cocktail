0961e5c4601c7baba63150c11b5df87423fd8cf8
Prasoon M <prasoon@aufaitux.com>
2025-06-28 13:11:19 +0530

    feat : Added animation in contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,44 @@
+import { useGSAP } from '@gsap/react'
 import { openingHours, socials } from '../../constants/index.js'
+import gsap from 'gsap'
+import { SplitText } from 'gsap/all'
+
+gsap.registerPlugin(SplitText);
 
 const Contact = () => {
+
+	useGSAP(() => {
+
+		const titleSplit = SplitText.create('#contact h2', {
+			type: 'words'
+		})
+		const timeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: '#contact',
+				start: 'top center',
+			},
+			ease: 'power1.inOut'
+		})
+		timeline
+			.from(titleSplit.words, {
+				opacity: 0,
+				yPercent: 100,
+				stagger: 0.02
+			})
+			.from('#contact h3,#contact p', {
+				opacity: 0,
+				yPercent: 100,
+				stagger: 0.02
+			})
+			.to('#f-right-leaf', {
+				y: '-50', duration: 1, ease: 'power1.inOut'
+			})
+			.to('#f-left-leaf', {
+				y: '-50', duration: 1, ease: 'power1.inOut'
+			}, '<')
+
+	}, [])
+
 	return (
 		<footer id="contact">
 			<img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
